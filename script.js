22291c89d17a3b852b24b225dd68d201870630b1
dave8git <dave8git@gmail.com>
2025-01-12 22:10:26 +0100

    WIP

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+// typescript assingment #1
 var btnElem = document.querySelector('.sum-btn');
 var numInput = document.querySelector('.num-input');
 function sum(a, b) {
     console.log(a + b);
 }
-btnElem.addEventListener('click', function () { sum(5, numInput.value); });
+btnElem.addEventListener('click', function () { sum(5, +numInput.value); });
--- script.ts
+++ script.ts
@@ -1,8 +1,24 @@
-const btnElem = document.querySelector('.sum-btn') as HTMLButtonElement;
-const numInput = document.querySelector('.num-input') as HTMLInputElement;
-
-function sum(a, b) {
-    console.log(a + b);
-}
-
-btnElem.addEventListener('click', () => { sum(5, numInput.value) });
+{
+    const persons: string[] = [];
+  
+    function addPerson(firstName: string, lastName: string) {
+      const name: string = firstName + lastName;
+      persons.push(name);
+    }
+  
+    let firstName: string = 'John';
+    let lastName: string = 'Doe';
+    let age: number = 18;
+  
+    const person: {
+      firstName: string,
+      lastName: string
+    } = {
+        firstName,
+        lastName
+    }
+  
+    addPerson(person.firstName, person.lastName);
+    addPerson('Amanda', 'Doe');
+    addPerson('Thomas', 'Jefferson');
+  }
